beecf1db56c0f532f18f926077b7f8db41429f66
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 10:19:33 +0100

    Implemented anonymous1, tests return true.

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -74,7 +76,16 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
+        
         // TODO
+        Collections.sort(denzelUsers, new Comparator<User>() {
+			@Override
+			public int compare(User o1, User o2) {
+				if(o1.getAge()<o2.getAge()) return -1;
+				else if(o1.getAge()==o2.getAge()) return 0;
+				else return 1;
+			}});
+        
         /*
          * expected Result
          */
@@ -105,7 +116,15 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        
+        Collections.sort(rossiUsers, new Comparator<User>() {
+			@Override
+			public int compare(User o1, User o2) {
+				if(o1.getAge()<o2.getAge()) return 1;
+				else if(o1.getAge()==o2.getAge()) return 0;
+				else return -1;
+			}});
+        
         /*
          * expected Result
          */
